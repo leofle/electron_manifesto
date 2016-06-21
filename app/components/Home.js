ffd62c9bed3b6acda459ef10c7ce990475c2c742
Lio Fleishman <leofle@gmail.com>
2016-06-21 22:00:49 +0300

    adding copyright color

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,15 +3,20 @@ import { Link } from 'react-router';
 import styles from './Home.css';
 
 
+
 export default class Home extends Component {
+
   render() {
+    var heartStyle = {
+      color: 'red'
+    }
     return (
       <div>
         <div className={styles.container}>
           <h2>Ux Manifesto</h2>
           <Link to="/manifesto"> Manifesto</Link>
           </div>
-          <p className={styles.paragraph}>Made with <span className="fa fa-heart"></span> by Lio Fleishman all rights reserved to ClickSoftware Inc. 2016</p>
+          <p className={styles.paragraph}>Made with <span style={heartStyle} className="fa fa-heart" ></span> by Lio Fleishman all rights reserved to ClickSoftware Inc. 2016</p>
       </div>
     );
   }
